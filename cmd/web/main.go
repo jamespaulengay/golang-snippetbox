d5ca968419a0fa4de6546eea2495a2c20796ad26
James Paul Engay <jamespaul.engay@tup.edu.ph>
2022-11-04 14:08:46 +0800

    wip: db integration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,42 +1,70 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"snippetbox.jamespaul.com/internal/models"
 )
 
-// a) Improved Logger
+// a) Improved Logger // Inject dependencies
 type application struct { 
 	errorLog *log.Logger 
 	infoLog *log.Logger
+	snippets *models.SnippetModel
 }
 
+
 func main() {
-	// Command-line flags
-	addr := flag.String("addr", ":4000", "HTTP network address") 
-
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	
 	flag.Parse()
-
+	
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	
+	db, err := openDB(*dsn) 
 
-	// Initialize a new instance of our application struct, containing the 
+	if err != nil {
+		errorLog.Fatal(err) 
+	}
+
+	defer db.Close()
+
+	// Initialize a models.SnippetModel instance and add it to the application 
 	// dependencies.
 	app := &application{
 		errorLog: errorLog,
-		infoLog: infoLog, 
+		infoLog: infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{ 
 		Addr: *addr,
 		ErrorLog: errorLog,
-		// Call the new app.routes() method to get the servemux containing our routes.
-		Handler: app.routes(),
+		Handler: app.routes(), 
 	}
 
 	infoLog.Printf("Starting server on %s", *addr) 
-	err := srv.ListenAndServe() 
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// for a given DSN.
+func openDB(dsn string) (*sql.DB, error) { 
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err 
+	}
+	
+	if err = db.Ping(); err != nil { 
+		return nil, err
+	}
+
+	return db, nil 
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module snippetbox.jamespaul.com
 
 go 1.19
+
+require github.com/go-sql-driver/mysql v1.6.0
